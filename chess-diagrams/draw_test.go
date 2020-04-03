b4bdb7d93d706f53c8c62d2406e758368f712664
StefanZ <stefan.zoerner@embarc.de>
2020-04-03 20:19:38 +0200

    package name adjusted

--- chess-diagrams/draw_test.go
+++ chess-diagrams/draw_test.go
@@ -1,14 +1,27 @@
-package chessdiagrams
+package main
 
 import (
 	"testing"
 )
 
+const startingPosition = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"
+
 func TestDrawDiagramForFen(t *testing.T) {
-	fen := "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"
+	fen := startingPosition
 	img := drawDiagramForFen(fen)
 
-	if img == nil {
-		t.Fail()
+	size := img.Bounds().Size()
+	if size.X != BoardSize {
+		t.Error("wrong diagram size X")
+	}
+	if size.Y != BoardSize {
+		t.Error("wrong diagram size Y")
+	}
+}
+
+func BenchmarkDrawDiagramForFen(b *testing.B) {
+	fen := startingPosition
+	for i := 0; i < b.N; i++ {
+		drawDiagramForFen(fen)
 	}
 }
